f80a924ceac205e6da9407b460624a69b77ed983
Bhanu <bhanupratapprasad999@gmail.com>
2024-05-27 23:50:37 +0530

    middleware

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Backend\CategoryController;
+use App\Http\Middleware\RoleMiddleware;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->middleware('role:admin');
 
 
 Route::get('add-category', [CategoryController::class, "AddCategory"]);
